816aee44a2c8f3120f2d015c2ede61a693bc5230
BoDelak <102085378+BoDelak@users.noreply.github.com>
2022-03-22 17:05:45 +0000

    fix reconnect and quit option. add more vibrations

--- bpserver/bpserver/Program.cs
+++ bpserver/bpserver/Program.cs
@@ -64,96 +64,100 @@ namespace bpclient
             server.Start();
             Console.WriteLine("Server started on 127.0.0.1:54321.{0}Waiting for a connection", Environment.NewLine);
 
-            TcpClient client = server.AcceptTcpClient();
-            NetworkStream stream = client.GetStream();
-            Console.WriteLine("A client connected.");
-            while (true)
+            do
             {
-                while (!stream.DataAvailable)
+                TcpClient client = server.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("A client connected.");
+                while (true)
                 {
-                    if (!IsConnected(client)) break;
-                }
-                if (!IsConnected(client)) break;
-                while (client.Available < 3) ; // match against "get"
-
-                try
-                {
-                    Byte[] bytes = new byte[client.Available];
-                    stream.Read(bytes, 0, bytes.Length);
-                    string s = Encoding.UTF8.GetString(bytes);
-
-                    if (Regex.IsMatch(s, "^GET", RegexOptions.IgnoreCase))
+                    while (!stream.DataAvailable)
                     {
-                        Console.WriteLine("=====Handshaking from client=====\n{0}", s);
-                        // 1. Obtain the value of the "Sec-WebSocket-Key" request header without any leading or trailing whitespace
-                        // 2. Concatenate it with "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" (a special GUID specified by RFC 6455)
-                        // 3. Compute SHA-1 and Base64 hash of the new value
-                        // 4. Write the hash back as the value of "Sec-WebSocket-Accept" response header in an HTTP response
-                        string swk = Regex.Match(s, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
-                        string swka = swk + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-                        byte[] swkaSha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(swka));
-                        string swkaSha1Base64 = Convert.ToBase64String(swkaSha1);
-
-                        // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
-                        byte[] response = Encoding.UTF8.GetBytes(
-                            "HTTP/1.1 101 Switching Protocols\r\n" +
-                            "Connection: Upgrade\r\n" +
-                            "Upgrade: websocket\r\n" +
-                            "Sec-WebSocket-Accept: " + swkaSha1Base64 + "\r\n\r\n");
-
-                        stream.Write(response, 0, response.Length);
+                        if (!IsConnected(client)) break;
                     }
-                    else
+                    if (!IsConnected(client)) break;
+                    while (client.Available < 3) ; // match against "get"
+
+                    try
                     {
-                        bool fin = (bytes[0] & 0b10000000) != 0,
-                            mask = (bytes[1] & 0b10000000) != 0; // must be true, "All messages from the client to the server have this bit set"
+                        Byte[] bytes = new byte[client.Available];
+                        stream.Read(bytes, 0, bytes.Length);
+                        string s = Encoding.UTF8.GetString(bytes);
 
-                        int opcode = bytes[0] & 0b00001111, // expecting 1 - text message
-                            msglen = bytes[1] - 128, // & 0111 1111
-                            offset = 2;
-
-                        if (msglen == 126)
+                        if (Regex.IsMatch(s, "^GET", RegexOptions.IgnoreCase))
                         {
-                            // was ToUInt16(bytes, offset) but the result is incorrect
-                            msglen = BitConverter.ToUInt16(new byte[] { bytes[3], bytes[2] }, 0);
-                            offset = 4;
-                        }
-                        else if (msglen == 127)
-                        {
-                            Console.WriteLine("TODO: msglen == 127, needs qword to store msglen");
-                            // i don't really know the byte order, please edit this
-                            // msglen = BitConverter.ToUInt64(new byte[] { bytes[5], bytes[4], bytes[3], bytes[2], bytes[9], bytes[8], bytes[7], bytes[6] }, 0);
-                            // offset = 10;
-                        }
+                            //Console.WriteLine("=====Handshaking from client=====\n{0}", s);
 
-                        if (msglen == 0)
-                        {
-                            //Console.WriteLine("msglen == 0");
-                        }
-                        else if (mask)
-                        {
-                            byte[] decoded = new byte[msglen];
-                            byte[] masks = new byte[4] { bytes[offset], bytes[offset + 1], bytes[offset + 2], bytes[offset + 3] };
-                            offset += 4;
+                            // 1. Obtain the value of the "Sec-WebSocket-Key" request header without any leading or trailing whitespace
+                            // 2. Concatenate it with "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" (a special GUID specified by RFC 6455)
+                            // 3. Compute SHA-1 and Base64 hash of the new value
+                            // 4. Write the hash back as the value of "Sec-WebSocket-Accept" response header in an HTTP response
+                            string swk = Regex.Match(s, "Sec-WebSocket-Key: (.*)").Groups[1].Value.Trim();
+                            string swka = swk + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+                            byte[] swkaSha1 = System.Security.Cryptography.SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(swka));
+                            string swkaSha1Base64 = Convert.ToBase64String(swkaSha1);
 
-                            for (int i = 0; i < msglen; ++i)
-                                decoded[i] = (byte)(bytes[offset + i] ^ masks[i % 4]);
+                            // HTTP/1.1 defines the sequence CR LF as the end-of-line marker
+                            byte[] response = Encoding.UTF8.GetBytes(
+                                "HTTP/1.1 101 Switching Protocols\r\n" +
+                                "Connection: Upgrade\r\n" +
+                                "Upgrade: websocket\r\n" +
+                                "Sec-WebSocket-Accept: " + swkaSha1Base64 + "\r\n\r\n");
 
-                            string text = Encoding.UTF8.GetString(decoded);
-                            //Console.WriteLine("{0}", text);
-                            bool validtext = await ProcessCommand(text);
-                            if (!validtext) break;
+                            stream.Write(response, 0, response.Length);
                         }
                         else
-                            Console.WriteLine("mask bit not set");
+                        {
+                            bool fin = (bytes[0] & 0b10000000) != 0,
+                                mask = (bytes[1] & 0b10000000) != 0; // must be true, "All messages from the client to the server have this bit set"
+
+                            int opcode = bytes[0] & 0b00001111, // expecting 1 - text message
+                                msglen = bytes[1] - 128, // & 0111 1111
+                                offset = 2;
+
+                            if (msglen == 126)
+                            {
+                                // was ToUInt16(bytes, offset) but the result is incorrect
+                                msglen = BitConverter.ToUInt16(new byte[] { bytes[3], bytes[2] }, 0);
+                                offset = 4;
+                            }
+                            else if (msglen == 127)
+                            {
+                                Console.WriteLine("TODO: msglen == 127, needs qword to store msglen");
+                                // i don't really know the byte order, please edit this
+                                // msglen = BitConverter.ToUInt64(new byte[] { bytes[5], bytes[4], bytes[3], bytes[2], bytes[9], bytes[8], bytes[7], bytes[6] }, 0);
+                                // offset = 10;
+                            }
+
+                            if (msglen == 0)
+                            {
+                                //Console.WriteLine("msglen == 0");
+                            }
+                            else if (mask)
+                            {
+                                byte[] decoded = new byte[msglen];
+                                byte[] masks = new byte[4] { bytes[offset], bytes[offset + 1], bytes[offset + 2], bytes[offset + 3] };
+                                offset += 4;
+
+                                for (int i = 0; i < msglen; ++i)
+                                    decoded[i] = (byte)(bytes[offset + i] ^ masks[i % 4]);
+
+                                string text = Encoding.UTF8.GetString(decoded);
+                                //Console.WriteLine("{0}", text);
+                                bool validtext = await ProcessCommand(text);
+                                if (!validtext) break;
+                            }
+                            else
+                                Console.WriteLine("mask bit not set");
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        break;
                     }
                 }
-                catch(Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    break;
-                }
-            }
+            } while (reconnect);
             server.Stop();
         }
 
@@ -165,10 +169,11 @@ namespace bpclient
                 {
                     case "0":
                         // player shoot
-                        await ControlDevice(0.5, 100);
+                        await ControlDevice(0.3, 100);
                         break;
                     case "1":
                         // explosion
+                        await ControlDevice(1.0, 200);
                         break;
                     case "2":
                         // fall
@@ -178,6 +183,7 @@ namespace bpclient
                         break;
                     case "4":
                         // holsterdischarge
+                        await ControlDevice(0.8, 100);
                         break;
                     case "5":
                         // shock
@@ -185,13 +191,13 @@ namespace bpclient
                         break;
                     case "6":
                         // shot
-                        await ControlDevice(0.5, 100);
+                        await VibratePattern(0.6, 100, 10, 5); // temp 5 repeats. maybe in future get how many shots?
                         break;
                     case "7":
                         // shrapnel
                         break;
                     default:
-                        Console.WriteLine("Code does not match. Received: " + text);
+                        Console.WriteLine("Invalid code. Received: " + text);
                         return false;
                 }
                 return true;
@@ -251,6 +257,15 @@ namespace bpclient
             return;
         }
 
+        private static async Task VibratePattern(double strength, int time, int downtime, int repeats)
+        {
+            for(int i = 0; i < repeats; i++)
+            {
+                await ControlDevice(strength, time);
+                await Task.Delay(time + downtime);
+            }
+        }
+
         private static async Task Run()
         {
             client = new ButtplugClient("chungus");
@@ -263,7 +278,7 @@ namespace bpclient
             }
             void HandleDeviceRemoved(object aObj, DeviceRemovedEventArgs aArgs)
             {
-                Console.WriteLine($"Device connected: {aArgs.Device.Name}");
+                Console.WriteLine($"Device disconnected: {aArgs.Device.Name}");
             }
             client.DeviceAdded += HandleDeviceAdded;
             client.DeviceRemoved += HandleDeviceRemoved;
@@ -287,10 +302,10 @@ namespace bpclient
                 Console.WriteLine("1. Scan For Devices");
                 Console.WriteLine("2. Control Devices");
                 Console.WriteLine("3. Toggle auto reconnect (currently " + (reconnect ? "enabled" : "disabled") + ")");
-                Console.WriteLine("3. Quit");
+                Console.WriteLine("4. Quit");
                 Console.WriteLine("Choose an option: ");
                 if (!uint.TryParse(Console.ReadLine(), out var choice) ||
-                    (choice == 0 || choice > 3))
+                    (choice == 0 || choice > 4))
                 {
                     Console.WriteLine("Invalid choice, try again.");
                     continue;
@@ -310,6 +325,7 @@ namespace bpclient
                     case 4:
                         return;
                     default:
+                        Console.WriteLine("Invalid choice, try again.");
                         continue;
                 }
             }
